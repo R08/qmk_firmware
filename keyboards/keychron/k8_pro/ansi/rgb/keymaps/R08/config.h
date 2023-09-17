240a33f1d161602541939b6b284eeffac2cb24bd
Bobby <rob1101@gmail.com>
2023-09-17 00:51:49 -0400

    Added my K8Pro keymap

---
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/R08/config.h
@@ -0,0 +1,4 @@
+#define TAPPING_TERM 200
+#define DYNAMIC_TAPPING_TERM_INCREMENT 50
+#define QUICK_TAP_TERM 0
+#define RETRO_TAPPING // Send original key code if no other key is pressed in the term limit.
