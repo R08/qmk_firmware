10436336856c2fe60400a24ec2d51def311cc4a3
bobby <rob1101@gmail.com>
2024-12-18 22:08:32 -0500

    More layers and L_CAPS keymaps

--- keyboards/keychron/k8_pro/ansi/rgb/keymaps/R08/config.h
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/R08/config.h
@@ -1,4 +1,7 @@
-#define TAPPING_TERM 200
+#define TAPPING_TERM 100
 #define DYNAMIC_TAPPING_TERM_INCREMENT 50
 #define QUICK_TAP_TERM 0
 #define RETRO_TAPPING // Send original key code if no other key is pressed in the term limit.
+
+#define LEADER_PER_KEY_TIMING
+#define LEADER_TIMEOUT 200
