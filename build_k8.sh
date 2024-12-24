48e2ba4b10debc0aab1f6058a4b5fe2fa1ec5244
bobby <rob1101@gmail.com>
2024-12-24 00:32:22 -0500

    build script update and remove keymaps

--- build_k8.sh
+++ build_k8.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-build() {
-    qmk compile -kb keychron/k8_pro/ansi/rgb -km R08
-}
+qmk compile -kb keychron/k8_pro/ansi/rgb -km R08
+qmk flash -kb keychron/k8_pro/ansi/rgb -km R08
 
-deploy() {
-    qmk flash -kb keychron/k8_pro/ansi/rgb -km R08
-}
-
-all() {
-    build
-    deploy
-}
-
-"$@"
